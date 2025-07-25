07c13cc576081228fd9b327d4e1e4ad1c99501ee
Fabricio Pagliarini <fabriciopgl87@gmail.com>
2025-07-25 20:07:54 -0300

    :sparkles: Adjust folders

---
+++ src/EnergyManagement.Infraestructure/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using EnergyManagement.Application.Sensors.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnergyManagement.Infraestructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<SensorReading> SensorReadings => Set<SensorReading>();
+}
